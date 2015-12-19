263623919cc75b363963e588c87122f50f207062
maoma87 <mao8a87@gmail.com>
2015-12-19 10:30:57 -0500

    arreglo de menú horizontal
    arreglado el menú horizontal y optimización de las clases del html

--- rspnsv-menu/js/rspnsv-menu.js
+++ rspnsv-menu/js/rspnsv-menu.js
@@ -1,16 +1,60 @@
 var ancho_btn_responsive = 52;
 var speed = 400;
 
+//VARIABLES PARA MENU HORIZONTAL
+var menu_horizontal = $(".menu-horizontal");
 
 
 $(document).ready(function(){
 
-	var n_divs = $(".menu-horizontal").length;
+	/*var variable00 = 1;
+	var variable01 = "mivariable_"+variable00;
+	$(".box-title-menu").eq(0).addClass(variable01);
+
+	var n_divs = $(".box-title-menu").eq(0).find("div").length;
+	console.log(n_divs);
+	*/
+
+	for (var i = 0; i < menu_horizontal.length; i++) {
+		menu_horizontal.eq(i).addClass("mh_"+i);
+
+		var menu_i = $(".mh_"+i);
+
+		console.log(menu_i.find("div").length);
+
+		var tamanos_array = new Array();
+
+		for (var i2 = 0; i2 < menu_i.find("div").length; i2++) {
+			var each_width = menu_i.find("div").eq(i2).width();
+			tamanos_array.push(each_width);
+		}
+
+		var border_right = menu_i.find("div").css('border-right-Width')[0];
+		border_right = parseInt(border_right);
+
+
+		var tamano_maximo = Math.max.apply(Math, tamanos_array);
+		tamano_maximo = Math.round(tamano_maximo);
+		tamano_maximo = parseInt(tamano_maximo);
+
+		menu_i.find("div").css({
+			"width": tamano_maximo + "px"
+		});
+
+		var width_final = (tamano_maximo + border_right) * menu_i.find("div").length;
+
+		menu_i.css({
+			"width":  width_final + "px"
+		});
+	}
+
 
 	var menu_horizontal_fn = function(){
 		var n_divs = $(".menu-horizontal").length;
 		var tamanos_array = new Array();
 
+
+		//CICLO PARA ASIGNARLE AL ARRAY (each_width) LOS ANCHOS DE CADA UNO DE LOS DIVS
 		for(var i = 0; i < n_divs; i++){
 			var each_width = $(".menu-horizontal").eq(i).width();
 			tamanos_array.push(each_width);
@@ -20,9 +64,7 @@ $(document).ready(function(){
 		border_right = parseInt(border_right);
 
 		if (tamanos_array.length == 3) {
-			var tamano_maximo = Math.max.apply(Math, tamanos_array);
-			tamano_maximo = Math.round(tamano_maximo);
-			tamano_maximo = parseInt(tamano_maximo);
+
 
 			var width_final = (tamano_maximo + border_right) * 3;
 
@@ -43,7 +85,7 @@ $(document).ready(function(){
 
 	};
 
-	menu_horizontal_fn();
+	//menu_horizontal_fn();
 
 	$("#rspnsv-menu > ul > li > ul").css({
 		"display":"none",
